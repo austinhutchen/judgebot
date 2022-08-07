dbea54a9dad361b924307d4179d4f709ae754972
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2022-08-07 13:11:45 -0700

    commit

---
+++ bot.h
@@ -0,0 +1,60 @@
+
+#ifndef bot_h
+#define bot_h
+#include <iostream>
+using namespace std;
+class bot{
+public:
+bot(){
+ likeness_score=0;
+}
+int get_score(string &name){
+
+for(int i=0;i<name.size();i++){
+ // play favorites with letters in name using ASCII (use lowercase function for this)
+switch(name[i]){
+case 
+
+
+
+
+
+
+}
+
+// play favorites with length of name
+
+
+
+// play final favorites with specific names
+
+
+
+}
+
+
+
+
+
+
+}
+
+
+
+
+
+private:
+float likeness_score;
+
+
+
+
+
+
+
+
+
+
+};
+#endif
+
---
+++ driver.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "bot.h"
+
+
+int main(){
+
+
+
+
+
+
+
+
+
+
+
+}
