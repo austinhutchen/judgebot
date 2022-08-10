a94ebdbf0aba25668625180d4b4c9c55ae63506b
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2022-08-10 13:27:33 -0700

    mini update

--- bot.h
+++ bot.h
@@ -6,32 +6,56 @@ using namespace std;
 class bot{
 public:
 bot(){
- likeness_score=0;
+ likeness_score=5;
 }
+string lowercase(string input){
+ 
+for(int x=0;x<input.size();x++){
+if(input[x]>=65&&input[x]<=90){
+int e=input[x]+32;
+input[x]=e;
+}
+else{continue;}
+}
+// end of loop
+return input;
+}
+
 int get_score(string &name){
-
+ int c=0;
+ srand(time(NULL));
+ long random=rand()%11; // random from 0 to 11
+string username=lowercase(name);
 for(int i=0;i<name.size();i++){
- // play favorites with letters in name using ASCII (use lowercase function for this)
-switch(name[i]){
-case 
-
-
-
-
-
-
+// play favorites with letters in name using ASCII (use lowercase function for this)
+switch(username[i]){
+// cases depend on lowercase ascii value in range 97-122
+case 97 ... 100:
+// from a to d
+likeness_score+=0.5;
+break;
+case 101 ... 110:
+likeness_score-=0.5;
+break;
+case 111 ... 117:
+likeness_score--;
+break;
+case 118 ... 122:
+likeness_score++;
+break;
+default: if(c==0){cout << "Some parts of name not within proper character range." << endl;
+c++;}
+break;
 }
+// formula to convert within range of name size below
 
-// play favorites with length of name
-
-
-
-// play final favorites with specific names
-
-
-
+// play final formula with specific names and rand values
 }
-
+int length=name.size();
+if(length<4){
+ likeness_score+=0.5;
+}
+else{likeness_score--;}
 
 
 
@@ -45,7 +69,7 @@ case
 
 private:
 float likeness_score;
-
+// score evaluated from 0-10
 
 
 
--- driver.cpp
+++ driver.cpp
@@ -2,9 +2,13 @@
 #include "bot.h"
 
 
+
 int main(){
-
-
+ bot bot1=bot();
+ string name;
+cout << "Enter your name below. No spaces." << endl;
+cin>>name;
+bot1.get_score(name);
 
 
 
